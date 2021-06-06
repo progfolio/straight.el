dc9159cb1dcffd1d043305e54442cc04ec827fd3
Nicholas Vollmer <iarchivedmywholelife@gmail.com>
2021-06-06 07:41:19 -0400

    straight--load-build-cache: decode unicode
    Decode unicode so that we're not spuriously rebuilding packages
    which use unicode characters.
    
    See: #780

--- straight.el
+++ straight.el
@@ -3971,8 +3971,8 @@ empty values (all packages will be rebuilt, with no caching)."
       (with-temp-buffer
         ;; Using `insert-file-contents-literally' avoids
         ;; `find-file-hook', etc.
-        (insert-file-contents-literally
-         (straight--build-cache-file))
+        (insert-file-contents-literally (straight--build-cache-file))
+        (decode-coding-region (point-min) (point-max) 'utf-8)
         (let ((version (read (current-buffer)))
               (last-emacs-version (read (current-buffer)))
               (build-cache (read (current-buffer)))
